87313a238fd221376a7d0351ed4e84de4045b427
ma-za-kpe <makpalyy@gmail.com>
2023-03-31 23:00:42 +0300

    remove checkstyle for java

--- tools/pre-push.sh
+++ tools/pre-push.sh
@@ -2,10 +2,9 @@
 
 # This script will run the pre-push checks in the given order
 # - ktlint
-# - checkstyle
-# - (others in the future)
+# - (others in the future like checkstyle)
 
-if bash tools/ktlint_lint_check.sh && bash tools/checkstyle_lint_check.sh ; then
+if bash tools/ktlint_lint_check.sh ; then
   echo "All checks passed successfully"
   exit 0
 else
