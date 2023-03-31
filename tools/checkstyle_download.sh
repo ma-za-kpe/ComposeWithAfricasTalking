90c4b9288ffb556ecb3aeee7dd21aa035e5b40e6
ma-za-kpe <makpalyy@gmail.com>
2023-03-31 22:54:20 +0300

    update checkstyle-10.7.0-all

--- tools/checkstyle_download.sh
+++ tools/checkstyle_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download checkstyle
-CHECKSTYLE="10.7.0"
+CHECKSTYLE="10.9.3"
 echo Using Checkstyle version $CHECKSTYLE
 curl -sSLOC - https://github.com/checkstyle/checkstyle/releases/download/checkstyle-$CHECKSTYLE/checkstyle-$CHECKSTYLE-all.jar
 chmod a+x checkstyle-$CHECKSTYLE-all.jar
